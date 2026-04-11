8259cca01d89bc3fc1268bc56b3501238d54f76f
Krianne Paul Lagleva <kriannepaullagleva7@gmail.com>
2026-04-12 06:28:49 +0800

    Initial commit - Laravel 12 with Tailwind

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,7 +16,6 @@
         @endif
     </head>
     <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
-        <h1 class="text-red-500 text-3xl font-bold">Tailwind Test</h1>
         <header class="w-full lg:max-w-4xl max-w-[335px] text-sm mb-6 not-has-[nav]:hidden">
             @if (Route::has('login'))
                 <nav class="flex items-center justify-end gap-4">
